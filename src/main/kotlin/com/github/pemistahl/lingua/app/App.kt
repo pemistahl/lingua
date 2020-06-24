cbdd6c420c7db9e5988de89d9d2506e2692ff945
Peter M. Stahl <pemistahl@gmail.com>
2020-06-24 11:26:49 +0200

    Fix App.kt (#61)

--- src/main/kotlin/com/github/pemistahl/lingua/app/App.kt
+++ src/main/kotlin/com/github/pemistahl/lingua/app/App.kt
@@ -18,7 +18,7 @@ package com.github.pemistahl.lingua.app
 
 import com.github.pemistahl.lingua.api.IsoCode639_1
 import com.github.pemistahl.lingua.api.LanguageDetectorBuilder
-import com.github.pemistahl.lingua.api.LanguageDetectorBuilder.Companion.fromAllBuiltInLanguages
+import com.github.pemistahl.lingua.api.LanguageDetectorBuilder.Companion.fromAllLanguages
 import com.github.pemistahl.lingua.api.LanguageDetectorBuilder.Companion.fromIsoCodes639_1
 import java.io.Console
 import java.util.Scanner
@@ -114,7 +114,7 @@ private fun runApp() {
         }
     } else {
         println("Loading language models...")
-        detectorBuilder = fromAllBuiltInLanguages()
+        detectorBuilder = fromAllLanguages()
     }
 
     if (detectorBuilder != null) {
